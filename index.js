3235119d39844f2a231c1a860c002454ed768c6b
jumanah <jumanah.f.14@gmail.com>
2023-08-14 20:51:24 +0300

    add

---
+++ index.js
@@ -0,0 +1,18 @@
+// Sunday HW
+
+let names = ["jumanah" ,"jojo","Alhanouf","farah"]
+console.log(names);
+console.log(names)(2,2);
+
+// Unshift 1
+console.log(names.unshift("jumanah faisal")); 
+console.log(names);
+
+// push 2
+names.push("jumanah")
+console.log(names);
+
+// pop 3
+names.pop();
+names.push("jumanah")
+console.log(names);
